349ad85e4a36e7675fce5524286da13bc4ab66d4
GTS-88 <gtejadosanchez@safareyes.es>
2026-01-16 14:25:01 +0100

    carry on with html templates

--- rankvideogames/urls.py
+++ rankvideogames/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rankvideogames.views import go_home, show_games
-
+from rankvideogames.views import go_home, show_games, go_login
 
 urlpatterns = [
-    path('', go_home, name='go_home'),
-    path('videogames/', show_games, name='go_game'),
+    path('', go_login, name='go_login'),
+    path('games/', show_games, name='go_game'),
+    path('login', go_login, name='go_login'),
 ]
--- rankvideogames/views.py
+++ rankvideogames/views.py
@@ -5,11 +5,13 @@ from rankvideogames.models import VideoGame
 
 # Create your views here.
 def go_home(request):
-    return render(request, "inicio.html")
+    return render(request, "home.html")
 
 
 def show_games(request):
-
     #findAll
     list_games = VideoGame.objects.all()
     return render(request, "game.html", {"games": list_games})
+
+def go_login(request):
+    return render(request, "login.html")
